d823c266bd625c8edf652ce831661402c0be3270
Gurvansh Singh <gurvanshsinghmakkar@gmail.com>
2025-09-27 16:22:17 -0400

    e

--- OpenGL/WindowController.cpp
+++ OpenGL/WindowController.cpp
@@ -22,7 +22,7 @@ void WindowController::NewWindow()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
-	M_ASSERT((m_window = glfwCreateWindow(1024, 768, "Sample Scene", NULL, NULL)) != nullptr, "Failed to open GLFW window.");
+	M_ASSERT((m_window = glfwCreateWindow(glfwGetVideoMode(glfwGetPrimaryMonitor())->width, glfwGetVideoMode(glfwGetPrimaryMonitor())->height, "Sample Scene", NULL, NULL)) != nullptr, "Failed to open GLFW window.");
 	glfwMakeContextCurrent(m_window);
 }
 
